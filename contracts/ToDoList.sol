c3a1a2e6395437bcd475238e16c12f0d1bd371d5
Kartik Shah <kartik@scholarlab.in>
2023-04-16 16:12:36 +0530

    - first commit

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract ToDoList {
+
+    uint public taskCount = 0;
+
+    constructor(){
+
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var TodoList = artifacts.require("./ToDoList.sol");
+
+module.exports = function(deployer) {
+    deployer.deploy(TodoList);
+};
